8ad4fdb85a210c741e79952597d3001f8adad8be
GladkayaElena <elena_gladkaj@ukr.net>
2024-08-12 22:03:07 +0300

    Dz-1

---
+++ DZ-1-js/Dz-1.1/main.js
@@ -0,0 +1,32 @@
+<!-- - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.-->
+<!--Вивести кожну змінну за допомогою: console.log-->
+
+
+    let a = 'hello';
+    let b = 'owu';
+    let c = 'com';
+    let d = 'ua';
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+    let num1 = 1;
+    let num2 = 10;
+    let num3 = -999;
+    let num4 = 123;
+    let num5 = 3.14;
+    let num6 = 2.7;
+    let num7 = 16;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+
+    let yes = true;
+    let no = false;
+console.log(yes);
+console.log(no);
---
+++ DZ-1-js/Dz-1.2/main.js
@@ -0,0 +1,14 @@
+<!-- - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)-->
+
+
+    let firstName = 'Olena';
+    let middleName = 'Oleksiivna';
+    let lastName= 'Gladka';
+
+    let person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person)
+
+
+
+
